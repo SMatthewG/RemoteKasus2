8f30544a435136e1841d91bb45a515797648c3bb
SMatthewG <sammatthgun@gmail.com>
2024-11-29 09:57:45 +0700

    Tambah paragraph

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-        <meta name="Index" content="width=device-width, initial-scale=1">
-        <title>INDEX</title>
-</head>
-<body>
-	<li>Hello World!</li>
-	<li>Lorem Ipsum</li>
-</body>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+        	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="">
+        	<title>GIT Remote kasus 2</title>
+	</head>
+	<body>
+		<h1>Hello World!</h1>
+		<p>Lorem</p>
+		<p>Ipsum</p>
+	</body>
 </html>
